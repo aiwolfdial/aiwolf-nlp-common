5c6607f8e840d1785ad55e22590afbbe1603b8c4
nharu-0630 <root@nharu.dev>
2025-02-14 22:51:05 +0900

    [add] クライアントのコメントを追加

--- src/aiwolf_nlp_common/client.py
+++ src/aiwolf_nlp_common/client.py
@@ -10,6 +10,27 @@ from aiwolf_nlp_common.packet.packet import Packet
 
 
 class Client:
+    """対戦サーバーとの通信を行うクライアントクラス.
+
+    Attributes:
+        url (str): 対戦サーバーのURL.
+        token (str | None): 対戦サーバーに接続するためのトークン.
+        socket (websocket.WebSocket): WebSocketクライアント.
+
+    Args:
+        url (str): 対戦サーバーのURL.
+        token (str | None): 対戦サーバーに接続するためのトークン.
+
+    Examples:
+        >>> from aiwolf_nlp_common.client import Client
+        >>> client = Client("ws://localhost:10000", "token")
+        >>> client.connect()
+        >>> packet = client.receive()
+        >>> print(packet.request)
+        Request.INITIALIZE
+        >>> client.close()
+    """
+
     def __init__(self, url: str, token: str | None) -> None:
         super().__init__()
         websocket.enableTrace(traceable=False)
@@ -23,12 +44,18 @@ class Client:
             self.headers.append(f"Authorization: Bearer {self.token}")
 
     def connect(self) -> None:
+        """対戦サーバーに接続します."""
         self.socket.connect(
             self.url,
             header=self.headers,
         )
 
     def receive(self) -> Packet:
+        """対戦サーバーからパケットを受信します.
+
+        Returns:
+            Packet: 受信したパケット.
+        """
         resp = self.socket.recv()
         resp_str = ""
         if isinstance(resp, (bytes, bytearray, memoryview)):
@@ -39,9 +66,15 @@ class Client:
         return Packet.from_dict(resp_dict)
 
     def send(self, req: str) -> None:
+        """対戦サーバーにリクエストを送信します.
+
+        Args:
+            req (str): 送信するリクエスト.
+        """
         if not req.endswith("\n"):
             req += "\n"
         self.socket.send(req)
 
     def close(self) -> None:
+        """対戦サーバーとの接続を閉じます."""
         self.socket.close()
